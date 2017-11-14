60a7cd59e0d58849f9199dce98f0e8ce88e9cbcc
Antonio Pereira <antonio6745.p@gmail.com>
2017-11-14 07:47:53 -0200

    DeleteAll

--- CacaRobos/src/main/java/br/sp/cacarobos/dao/GenericDao.java
+++
@@ -1,9 +0,0 @@
-package br.sp.cacarobos.dao;
-import java.util.List;
-public interface GenericDao<T> {
-	public void create(T t);
-	public T read(Long t);
-	public void update(T t);
-	public void delete(Long t);
-	public List<T> listAll();
-}
--- CacaRobos/src/main/java/br/sp/cacarobos/model/Manager.java
+++
@@ -1,3 +0,0 @@
-package br.sp.cacarobos.model;
-public class Manager extends Person{
-}
